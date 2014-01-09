b9dedf777c418d927afbc4d988af88ce6ffc2c4e
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2014-01-10 00:07:33 +0200

    fix working

--- kot/java/Demo.java
+++ kot/java/Demo.java
@@ -2,13 +2,13 @@
  * Created by ivan on 1/10/14.
  */
 public class Demo {
-    public void do(){
-        My my = new My(12);
-        int value = my.getSomeValue();
-        System.out.print("" + value);
-    }
-
-    public static void main(String args[]){
-        new Demo().do();
-    }
+//    public void do(){
+//        My my = new My(12);
+//        int value = my.getSomeValue();
+//        System.out.print("" + value);
+//    }
+//
+//    public static void main(String args[]){
+//        new Demo().do();
+//    }
 }
